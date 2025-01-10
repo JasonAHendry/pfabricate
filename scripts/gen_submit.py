2626a38e750b6dcdf0396179aac4792e94de8b11
JasonAHendry <jason.ahendry@gmail.com>
2025-01-10 10:59:29 +0100

    Update to work on Slurm, rather than depreciated BMRC.

--- scripts/gen_submit.py
+++ scripts/gen_submit.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from pfabricate.util.generic import produce_dir
 
 
-SOURCE_COMMAND = "qsub cluster/bmrc_pfabricate.sh"
+SOURCE_COMMAND = "sbatch slurm/pfabricate_filter.slurm"
 CHROMOSOMES = range(1, 15)
 
 
@@ -22,25 +22,38 @@ class DatasetVCF:
     vcf_dir: str
     vcf_template: str
 
-    def get_chrom_vcf(self, chrom_int):
-        return f"{self.vcf_dir}/{self.vcf_template.format(chrom=chrom_int)}"
+    def get_chrom_vcf(self, chrom_int: int):
+        chrom_str = f"{chrom_int:02d}"
+        return f"{self.vcf_dir}/{self.vcf_template.format(chrom=chrom_str)}"
+    
+    def get_chrom_vcf_arrayjob(self, bash_variable: str = "$CHROM"):
+        return f"{self.vcf_dir}/{self.vcf_template.format(chrom=bash_variable)}"
 
+
+dataset_pf3k_local = DatasetVCF(
+    name="pf3k_local",
+    metadata="data/pf3k/metadata/pf3k_release_5_metadata_20190910_cleaned.csv",
+    vcf_dir="data/pf3k/vcfs",
+    vcf_template="SNP_INDEL_Pf3D7_{chrom}_v3.high_quality_biallelic_snps.vcf.gz",
+)
 
 dataset_pf3k = DatasetVCF(
     name="pf3k",
-    metadata="data/pf3k/metadata/pf3k_release_5_metadata_20190910_cleaned.csv",
-    vcf_dir="data/pf3k/vcfs",
-    vcf_template="SNP_INDEL_Pf3D7_{chrom:02d}_v3.high_quality_biallelic_snps.vcf.gz",
+    metadata="/u/jash/resource_data/malariagen/pf3k/metadata/pf3k_release_5_metadata_20190910_cleaned.csv",
+    vcf_dir="/u/jash/resource_data/malariagen/pf3k/vcfs/filtered",
+    vcf_template="SNP_INDEL_Pf3D7_{chrom}_v3.combined.filtered.vqslod6.biallelic_snp.vcf.gz"
+
+
 )
 
 dataset_pf6 = DatasetVCF(
     name="pf6",
     metadata="data/pf6/metadata/Pf_60_candidate_public_metadata_20170919_cleaned.csv",
     vcf_dir="data/pf6/vcfs",
-    vcf_template="Pf_60_public_Pf3D7_{chrom:02d}_v3.high_quality_biallelic_snps.vcf.gz",
+    vcf_template="Pf_60_public_Pf3D7_{chrom}_v3.high_quality_biallelic_snps.vcf.gz",
 )
 
-DATASET_COLLECTION = {"pf3k": dataset_pf3k, "pf6": dataset_pf6}
+DATASET_COLLECTION = {"pf3k": dataset_pf3k, "pf6": dataset_pf6, "pf3k_local": dataset_pf3k_local}
 
 
 # --------------------------------------------------------------------------------
@@ -54,6 +67,20 @@ def get_samples_in_region(metadata, region_dt):
                     for k, v in region_dt.items()])
     df = metadata.query(qry)
     return df["sample"]
+
+
+# --------------------------------------------------------------------------------
+# Writing output file
+#
+# --------------------------------------------------------------------------------
+
+def load_format_write(input_file: str, output_file: str, **kwargs) -> None:
+    """Load a file that has named formating fields, e.g. {job_name}, format it, and write"""
+    
+    input_str = "".join(open(input_file, "r").readlines())
+    output_str = input_str.format(**kwargs)
+    with open(output_file, "w") as output:
+        output.write(output_str)
 
 
 # --------------------------------------------------------------------------------
@@ -109,18 +136,27 @@ def main(dataset, population, country, site, year):
     print(f"  E.g.: {','.join(samples[:5])}...")
 
     # Define submission script
-    submit_fn = f"submit_pfabricate-filter-{region}.sh"
+    submit_fn = f"submit_pfabricate-filter-{region}.slurm"
     print(f"Writing submission script to:\n  {submit_fn}")
-    with open(submit_fn, "w") as file:
-        for c in CHROMOSOMES:
-            in_vcf = vcf_dataset.get_chrom_vcf(c)
-            out_vcf = (
-                f"{vcf_dir}/{os.path.basename(in_vcf).replace('.vcf', '.filter.vcf')}"
-            )
-            file.write(f"{SOURCE_COMMAND} filter -i {in_vcf} -o {out_vcf} -s {sample_str}\n")
+
+    # Prepare formatting
+    array_str = ",".join([str(c) for c in CHROMOSOMES])
+    in_vcf_template = vcf_dataset.get_chrom_vcf_arrayjob()
+    out_vcf = (
+        f"{vcf_dir}/{os.path.basename(in_vcf_template).replace('.vcf', '.filter.vcf')}"
+    )
+
+    load_format_write(
+        input_file="slurm/pfabricate-filter.slurm",
+        output_file=submit_fn,
+        array_str=array_str,
+        in_vcf_template=in_vcf_template,
+        out_vcf=out_vcf,
+        sample_str=sample_str
+    )
+
     print("Done.\n")
     os.chmod(submit_fn, 0o775)
-
 
 if __name__ == "__main__":
     main()
